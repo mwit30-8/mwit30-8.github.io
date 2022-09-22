12c14831c74b2e6a159b4dcd8082b17847fc6e40
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-09-22 16:42:44 +0700

    Add Google Analytics.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import ViteRadar from 'vite-plugin-radar';
 
-export default defineConfig(() => {
-  return {
-    ssr: { target: "node", format: "cjs" },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-  };
+export default defineConfig((config: UserConfig) => {
+  config.ssr = { ...config.ssr, target: "node", format: "cjs" };
+  config.plugins = config.plugins ?? [];
+  config.plugins.push(qwikCity());
+  config.plugins.push(qwikVite());
+  config.plugins.push(tsconfigPaths());
+  if (process.env.ANALYTICS_MEASUREMENT_ID)
+    config.plugins.push(
+      ViteRadar({
+        analytics: {
+          id: process.env.ANALYTICS_MEASUREMENT_ID
+        }
+      })
+    );
+  return config;
 });
