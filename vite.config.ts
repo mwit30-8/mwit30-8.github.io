5ec37bf2d13d98ee9730f2b72212b555e8763ab7
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-09-22 17:03:12 +0700

    Remove Google Analytics.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import ViteRadar from "vite-plugin-radar";
 
 export default defineConfig((config: UserConfig) => {
   config.ssr = { ...config.ssr, target: "node", format: "cjs" };
@@ -10,13 +9,5 @@ export default defineConfig((config: UserConfig) => {
   config.plugins.push(qwikCity());
   config.plugins.push(qwikVite());
   config.plugins.push(tsconfigPaths());
-  if (process.env.ANALYTICS_MEASUREMENT_ID)
-    config.plugins.push(
-      ViteRadar({
-        analytics: {
-          id: process.env.ANALYTICS_MEASUREMENT_ID,
-        },
-      })
-    );
   return config;
 });
